1ca07313904fd2d4275b75cf58498567e384f11a
webmeisterT <oluwatoyinakinlua@gmail.com>
2023-05-06 13:31:04 +0100

    first commit

---
+++ pseudocode.php
@@ -0,0 +1,16 @@
+<?php
+// Create a function to boil water
+// inside the function:
+    // Create a pot 
+    // Measure the water in liters
+    // Fill the pot with water
+    // make use of the type of heat you want(It can be high, medium or low)
+    // Heat the pot with the water in it on heat type (i.e. Boil water in a pot)
+// Create a function to cook indomie
+// inside the function:
+    // Measure the indomie per searchet
+    // Add the Indomie quantity to the boiled water
+    // set time to cook the indomie in seconds
+    // cook the indomie
+
+    
